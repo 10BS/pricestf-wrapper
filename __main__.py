ad0d6eb5aa3788ba462a6bef8b70df9b602048b5
reue <reue5118@protonmail.com>
2024-09-28 16:22:50 +1000

    Update

--- __main__.py
+++ __main__.py
@@ -1,17 +1,29 @@
 from conversion import Conversion
 from prices import Price
+from item_model import Items
 
 
 def main(sku: str) -> None:
+
     price = Price()
-    item = price.get_price(sku, 1, 1, "DESC")["items"][0]
-    ref_buy = Conversion.do(item["buyHalfScrap"])
-    ref_sell = Conversion.do(item["sellHalfScrap"])
-    print(
-        "\nMann Co. Supply Crate Key", f"SELL:\t{ref_sell}", f"BUY:\t{ref_buy}", sep="\n"
+    item_data = price.get_price(sku, 1, 1, "DESC")
+    item = Items(**item_data)
+
+    key_buy = item.items[0].buy_keys
+    key_sell = item.items[0].sell_keys
+
+    ref_buy = Conversion.do(item.items[0].buy_half_scrap)
+    ref_sell = Conversion.do(item.items[0].sell_half_scrap)
+
+    return print(
+        f"\n{sku}",
+        f"SELL:\t{key_sell} key {ref_sell} ref",
+        f"BUY:\t{key_buy} key {ref_buy} ref\n",
+        sep="\n"
     )
 
 
 if __name__ == "__main__":
-    data = input("Enter SKU: ")
-    main(str(data))
+    while True:
+        data = input("Enter SKU: ")
+        main(str(data))
